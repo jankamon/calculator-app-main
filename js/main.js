48c1507c62432590383a07d635aa86ffc077442b
janekk <33941864+jankamon@users.noreply.github.com>
2023-03-06 20:46:21 +0100

    work in progress

--- js/main.js
+++ js/main.js
@@ -2,3 +2,31 @@
 import "./theme.js";
 
 // Calculator
+const numButtons = document.getElementsByClassName('num-btn');
+const numBtn = [...numButtons];
+const signButtons = document.getElementsByClassName('sign-btn');
+const signBtn = [...numButtons];
+const calc = document.getElementById('calc');
+
+let numArr = [];
+let sign;
+
+numBtn.forEach(element => {
+    element.addEventListener('click', (value) => {
+        numArr.push(element.value);
+        console.log([...numArr]);
+    })
+});
+
+signBtn.forEach(element => {
+    element.addEventListener('click', (value) => {
+        numArr.push(element.value);
+        console.log([...numArr]);
+    })
+});
+
+calc.onclick = calculate(firstArr, secondArr);
+
+const calculate = () => {
+
+}
